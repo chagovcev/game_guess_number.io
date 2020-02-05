7ef3daee692dcbfbfcbbc045c05d80c36e5b5d1a
Roman Chagovtsev <4agovtsev@gmail.com>
2020-02-05 17:26:39 +0100

    Создана игра

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 'use strict'
+let randomNumber = Math.floor(Math.random() * 100);
+    console.log(randomNumber);
+
+let guessNumber = function(){
+    
+    let userInput = prompt('Угадай');
+
+        if(userInput){
+            let myV = Number(userInput);
+                
+            if(myV === randomNumber){
+                alert('Угадал');
+                let repite = confirm('Хотите сыграть еще?')
+                if (repite === true){
+                    guessNumber();
+                }
+                
+            }
+            else if(myV > randomNumber) {
+                alert('Больше');
+                guessNumber();
+            }
+            else if(myV < randomNumber) {
+                alert('Меньше');
+                guessNumber();
+            }
+        }
+    
+}
+guessNumber();
